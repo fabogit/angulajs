11a25166f20dd69ef921f7189d24fff8189949ef
fabogit <fabo.dev.email@gmail.com>
2022-10-24 20:59:45 +0200

    #5

--- 04_data-binding&directives/app.js
+++ 04_data-binding&directives/app.js
@@ -3,22 +3,28 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', ['$scope', '$timeout', '$filter',
 	function ($scope, $timeout, $filter) {
 
-		$scope.name = 'interpolated string';
+		$scope.myVar = 'interpolated string waiting 3s...';
 		$scope.input = '';
-		$scope.maxChars = 5;
-		$scope.myList = []
+		$scope.charNum = 5;
+		$scope.myList = [];
+		$scope.rules = [
+			{ruleName: "Must be 5 characters"},
+			{ruleName: "Must be unique"},
+			{ruleName: "Must be lowercase"}
+		]
 
 		$scope.lowercaseInput = function () {
 			return $filter('lowercase')($scope.input);
 		};
 
+		$timeout(function () {
+			$scope.myVar = 'timeout done!';
+		}, 3000);
 
+		// watchers and digest loop, $scope.$apply() if need to include
 		$scope.$watch('input', function (newVal, oldVal) {
-			console.log(`Changed Old: ${oldVal}, New: ${newVal}`);
+			console.log(`Changed, Old: ${oldVal}, New: ${newVal}`);
 		});
 
-		$timeout(function () {
-			$scope.name = 'timeout';
-		}, 3000);
 	}
 ]);
